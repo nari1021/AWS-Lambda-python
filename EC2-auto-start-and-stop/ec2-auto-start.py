038680549c4ba21df9002b4849dc1aeda0a0402d
nari1021 <y.shhh000@gmail.com>
2021-11-01 10:14:39 +0900

    create ::ec2 auto start and stop function

---
+++ EC2-auto-start-and-stop/ec2-auto-start.py
@@ -0,0 +1,45 @@
+import boto3
+import botocore
+import json
+
+region = 'ap-northeast-2'
+ec2 = boto3.resource('ec2', region_name=region)
+
+def lambda_handler(event, context):
+    if turnOn() == 'success':
+        return {
+            'statusCode': 200,
+            'body': "SUCCESS"
+        }
+    else:
+        return {
+            "statusCode": 400,
+            "body": json.dumps(error)
+        }
+    
+def turnOn():
+    # 태그에 AutoStartStop 키와 true 값을 가지는 인스턴스를 찾음
+    instances = ec2.instances.filter(
+        Filters=[{
+            # 태그 이름이 다르다면 이 부분을 수정하면 된다.
+            # 현재 태그이름은 AutoStartStop 이고, 값은 true 이다.
+            'Name': 'tag:AutoStartStop',
+            'Values': ['true']
+        }]
+    )
+    
+    # 찾은 인스턴스들을 RunningInstances 변수에 넣음
+    RunningInstances = [instance.id for instance in instances]
+    
+    for instance in RunningInstances:
+        try:
+            # RunningInstances 변수에 담긴 인스턴스들을 시작 지킴
+            ec2.instances.start(InstanceIds=[instance])
+            print('Start Instances ID :: ' + str(instance))
+        except botocore.exceptions.ClientError as error:
+            logger.error(error)
+            return error
+        pass
+    
+    return 'success'
+    
---
+++ EC2-auto-start-and-stop/how-to-use.md
@@ -0,0 +1,25 @@
+# how to use this folder
+
+1. Login AWS console by your account.
+
+2. Make 2 AWS Lambda funtion.
+    - ec2-auto-start function
+    - ec2-auto-stop function
+
+3. Copy and Paste python file on Each function
+
+4. If you want to use different tag name, you might change tag name in each file.
+
+<hr/>
+
+# 이 폴더의 사용법
+
+1. AWS 콘솔에 자신의 계정으로 로그인을 합니다.
+
+2. AWS Lambda 함수를 2개 생성합니다.
+    - ec2-auto-start 함수 1개
+    - ec2-auto-stop 함수 1개
+
+3. 각각의 함수에 python 파일을 복사해서 붙여넣습니다.
+
+4. 각자 auto start/stop 을 위한 태그가 다르다면, 코드안의 태그를 수정하여 사용하면 됩니다.
